6ad7a8f883dba8b61daa76b7da71827e7505c301
Nader Khamis <naderkhamisabdelaziz@gmail.com>
2022-03-15 22:38:09 +0300

    header view, style has been modified

--- nk-website/src/client/index.js
+++ nk-website/src/client/index.js
@@ -3,9 +3,6 @@ var favIcon = document.getElementById('fav-icon'),
     headerLogo = document.getElementById('header-logo');
 favIcon.href = headerLogo.src = headerlogo;
 
-// Bootstrap
-import 'bootstrap/scss/bootstrap.scss'
-// /Bootstrap
 // Main-Style
 import '../client/styles/style.scss'
 // /Main-Style
