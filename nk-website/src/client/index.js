47ab36fdfbdd6614ed14c08734ab99468f1b9dc9
Nader Khamis <naderkhamisabdelaziz@gmail.com>
2022-03-18 18:33:18 +0300

    landing view, style has been settled

--- nk-website/src/client/index.js
+++ nk-website/src/client/index.js
@@ -1,7 +1,14 @@
-import headerlogo from "../client/media/headerlogo.png"
-var favIcon = document.getElementById('fav-icon'),
-    headerLogo = document.getElementById('header-logo');
-favIcon.href = headerLogo.src = headerlogo;
+import header_logo from "../client/media/headerlogo.png"
+import landing_image from "../client/media/nader-khamis.png"
+import landing_border from "../client/media/landing-border.png"
+let favIcon = document.getElementById('fav-icon'),
+    headerLogo = document.getElementById('header-logo'),
+    landingImage = document.getElementById('nader-image'),
+    landingBorder = document.getElementById('landing-border');
+
+favIcon.href = headerLogo.src = header_logo;
+landingImage.src = landing_image;
+landingBorder.src = landing_border;
 
 // Main-Style
 import '../client/styles/style.scss'
